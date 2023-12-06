65d253bba8d6e2d80657fe5f817b505871d75ad0
mohammad.jelodar <mohammad.jelodar@gmail.com>
2023-12-06 18:54:14 +0330

    Modify MainViewModel to use GetAllRemindersUseCase

--- app/src/main/java/com/quadroidev/medic/presentation/features/main/MainViewModel.kt
+++ app/src/main/java/com/quadroidev/medic/presentation/features/main/MainViewModel.kt
@@ -1,11 +1,30 @@
 package com.quadroidev.medic.presentation.features.main
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.quadroidev.medic.core.components.base.BaseViewModel
+import com.quadroidev.medic.core.model.Habit
+import com.quadroidev.medic.domain.usecases.feature.createreminder.GetAllRemindersUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor() :BaseViewModel() {
+class MainViewModel @Inject constructor(
+    private val getAllRemindersUseCase: GetAllRemindersUseCase
+) :BaseViewModel() {
+
+    private val _habits: Channel<List<Habit>> = Channel()
+    val habits: Flow<List<Habit>> = _habits.receiveAsFlow()
+
+    fun getAllHabits() {
+        viewModelScope.launch {
+            getAllRemindersUseCase.getAllHabits().collect {
+                _habits.send(it)
+            }
+        }
+    }
 
 }
