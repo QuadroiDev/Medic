d9b9fa9eb7b07f379c9d3bd6a16cbcda022b243f
mohammad.jelodar <mohammad.jelodar@gmail.com>
2023-12-06 18:54:41 +0330

    Modify MainFragment to show all habits

--- app/src/main/java/com/quadroidev/medic/presentation/features/main/MainFragment.kt
+++ app/src/main/java/com/quadroidev/medic/presentation/features/main/MainFragment.kt
@@ -2,6 +2,10 @@ package com.quadroidev.medic.presentation.features.main
 
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.quadroidev.medic.R
 import com.quadroidev.medic.core.components.base.BaseFragment
 import com.quadroidev.medic.databinding.FragmentMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,9 +16,31 @@ class MainFragment : BaseFragment<FragmentMainBinding, MainViewModel>(
     MainViewModel::class
 ) {
 
+    private val adapter: MainAdapter by lazy { MainAdapter() }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.addReminder.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_createReminderFragment)
+        }
+        setAdapter()
+
+        observeHabits()
+    }
+
+    private fun setAdapter() {
+        binding.reminderRecyclerview.adapter = adapter
+        binding.reminderRecyclerview.layoutManager = LinearLayoutManager(requireContext())
+        adapter.submitList(emptyList()) // TODO: set data
+    }
+
+    private fun observeHabits() {
+        viewModel.getAllHabits()
+        launchWhen({
+            viewModel.habits.collect {habits ->
+                adapter.submitList(habits)
+            }
+        }, Lifecycle.State.CREATED)
     }
 }
