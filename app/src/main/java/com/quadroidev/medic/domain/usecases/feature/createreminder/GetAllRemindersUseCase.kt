e9c4d1c60224c232ebaeec39e2c8610db8f912e6
mohammad.jelodar <mohammad.jelodar@gmail.com>
2023-12-06 18:44:51 +0330

    Create GetAllRemindersUseCase

---
+++ app/src/main/java/com/quadroidev/medic/domain/usecases/feature/createreminder/GetAllRemindersUseCase.kt
@@ -0,0 +1,12 @@
+package com.quadroidev.medic.domain.usecases.feature.createreminder
+
+import com.quadroidev.medic.domain.repository.ReminderRepository
+import javax.inject.Inject
+
+class GetAllRemindersUseCase @Inject constructor(
+    private val reminderRepository: ReminderRepository) {
+
+    suspend fun getAllHabits() = reminderRepository.getAllHabits()
+
+
+}
